d8d1540d82e7448f95ccd94857e0f17873328b94
The-Estonian <Zaar2213@mail.com>
2023-05-22 13:16:04 +0300

    modified to-git-or-not-to-git-sh file

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,6 +1,3 @@
-requestedName=$(curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).name")
-requestedPower=$(curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).powerstats.power")
-requestedGender=$(curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).appearance.gender")
-echo $requestedName 
-echo $requestedPower 
-echo $requestedGender
+curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).name"
+curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).powerstats.power"
+curl https://01.kood.tech/assets/superhero/all.json | jq " .[] | select( .id == 170 ).appearance.gender"
