9867ce75fc50bd9b00844adac34771d5bcaa52a8
The-Estonian <Zaar2213@mail.com>
2023-05-23 10:30:44 +0300

    fixed teacher.sh

--- teacher.sh
+++ teacher.sh
@@ -1,5 +1,4 @@
-location=$(find -name "*mystery*")
-interview_number=$(grep "SEE INTERVIEW" $location/streets/Buckingham_Place |  cut -f 2 -d "#")
+interview_number=$(grep "SEE INTERVIEW" streets/Buckingham_Place |  cut -f 2 -d "#")
 echo $interview_number
 find . -name "interview-$interview_number" -exec cat {} +
 echo $MAIN_SUSPECT
