f1cfcf4c7119b44506cb4e877a413af7994e167c
The-Estonian <Zaar2213@mail.com>
2023-05-24 13:48:49 +0300

    Quest2/printcomb2 start

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for firstLoop := 0; firstLoop <= 9; firstLoop++ {
+		for secondLoop := 0; secondLoop <= 9; secondLoop++ {
+			fourthLoop := secondLoop + 1
+			for thirdLoop := firstLoop; thirdLoop <= 9; thirdLoop++ {
+				for ; fourthLoop <= 9; fourthLoop++ {
+					z01.PrintRune(rune(firstLoop + '0'))
+					z01.PrintRune(rune(secondLoop + '0'))
+					z01.PrintRune(' ')
+					z01.PrintRune(rune(thirdLoop + '0'))
+					z01.PrintRune(rune(fourthLoop + '0'))
+					if firstLoop < 9 || secondLoop < 9 || thirdLoop < 9 || fourthLoop < 9 {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+				fourthLoop = '0'
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
