55a37cfae5fd8ef20ad9bad84371872396ace175
The-Estonian <Zaar2213@mail.com>
2023-05-25 09:42:02 +0300

    Quest3/strlen adjusted rune modification

--- strlen.go
+++ strlen.go
@@ -1,8 +1,9 @@
 package piscine
 
 func StrLen(s string) int {
+	stringByte := []rune(s)
 	counter := 0
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(stringByte); i++ {
 		counter++
 	}
 	return counter
