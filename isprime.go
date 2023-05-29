52aa57944ecb684f1126fd5279340eab9213e54a
The-Estonian <Zaar2213@mail.com>
2023-05-29 14:46:38 +0300

    Quest4/isprime started

---
+++ isprime.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	num := 2
+	answer := true
+	for num <= nb/2 {
+		remainder := nb % num
+		if remainder != 0 {
+			num = num + 1
+		} else {
+			answer = false
+			break
+		}
+	}
+	return answer
+}
