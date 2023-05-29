f03183c7c2ef167174b00c95a22c659c950a1b5a
The-Estonian <Zaar2213@mail.com>
2023-05-29 11:18:53 +0300

    recursivepower edge case third fix

--- recursivepower.go
+++ recursivepower.go
@@ -4,8 +4,11 @@ func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if power == 0 {
+	if power == 0 && nb != 0 {
 		return nb
+	}
+	if nb == 0 && power == 0 {
+		return 1
 	}
 	if power == 1 {
 		return nb
