b53d2a3375452e5a70b488553d4968bf68949fbc
The-Estonian <Zaar2213@mail.com>
2023-05-29 15:52:50 +0300

    Quest4/findnextprime edge case second fix

--- findnextprime.go
+++ findnextprime.go
@@ -1,25 +1,7 @@
 package piscine
 
-func IsPrime2(nb int) bool {
-	if nb < 2 {
-		return false
-	}
-	num := 2
-	answer := true
-	for num <= nb/2 {
-		remainder := nb % num
-		if remainder != 0 {
-			num = num + 1
-		} else {
-			answer = false
-			break
-		}
-	}
-	return answer
-}
-
 func FindNextPrime(nb int) int {
-	if IsPrime2(nb) {
+	if IsPrime(nb) {
 		return nb
 	}
 	return FindNextPrime(nb + 1)
