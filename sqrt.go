710bfef9eb244f1bddd52e5c47d5f71b3bb7e8cc
The-Estonian <Zaar2213@mail.com>
2023-05-29 13:43:37 +0300

    Quest4/sqrt edge case fix

--- sqrt.go
+++ sqrt.go
@@ -1,15 +1,12 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb < 0 {
-		return 0
-	}
-	num := 1
+	x := 1
 	for i := 0; i < 10; i++ {
-		num = (num + nb/num) / 2
+		x = (x + nb/x) / 2
 	}
-	if num%2 == 1 {
+	if x%2 == 1 {
 		return 0
 	}
-	return num
+	return x
 }
