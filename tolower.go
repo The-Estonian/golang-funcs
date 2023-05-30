59d017093a7b333d9c6b126ddf8338768be523f3
The-Estonian <Zaar2213@mail.com>
2023-05-30 15:43:02 +0300

    Quest5/tolower started

---
+++ tolower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func ToLower(s string) string {
+	stringRune := []rune(s)
+	newString := ""
+	for i := 0; i < len(stringRune); i++ {
+		if stringRune[i] <= 90 && stringRune[i] >= 65 {
+			newString += string(stringRune[i] + 32)
+		} else if stringRune[i] <= 122 && stringRune[i] >= 97 {
+			newString += string(stringRune[i])
+		} else {
+			newString += string(stringRune[i])
+		}
+	}
+	return newString
+}
