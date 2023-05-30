81a276fe39cba3cfe249751e6024e6212e4928c3
The-Estonian <Zaar2213@mail.com>
2023-05-30 17:40:44 +0300

    Quest5/capitalize edge case fixed

--- capitalize.go
+++ capitalize.go
@@ -3,25 +3,28 @@ package piscine
 func Capitalize(s string) string {
 	stringRune := []rune(s)
 	returnString := ""
-	if stringRune[0] >= 97 {
-		returnString += string(stringRune[0] - 32)
-	} else {
-		returnString += string(stringRune[0])
-	}
-	for i := 1; i < len(stringRune); i++ {
-		if stringRune[i-1] == 32 {
+	counter := 0
+	for i := 0; i < len(stringRune); i++ {
+		if IsAlpha(string(stringRune[i])) {
+			counter++
+		} else {
+			counter = 0
+		}
+
+		if counter == 1 {
 			if stringRune[i] >= 97 && stringRune[i] <= 122 {
 				returnString += string(stringRune[i] - 32)
 			} else {
 				returnString += string(stringRune[i])
 			}
 		} else {
-			if stringRune[i] <= 90 || stringRune[i] >= 65 {
-				returnString += string(stringRune[i])
-			} else {
+			if stringRune[i] <= 90 && stringRune[i] >= 65 {
 				returnString += string(stringRune[i] + 32)
+			} else {
+				returnString += string(stringRune[i])
 			}
 		}
 	}
+
 	return returnString
 }
