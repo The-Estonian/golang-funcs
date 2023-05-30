12c1b3f1f35a1cbfe280226ca6c2794eea357640
The-Estonian <Zaar2213@mail.com>
2023-05-30 15:27:16 +0300

    Quest5/isprintable edge case fix

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	stringRune := []rune(s)
 	for i := 0; i < len(stringRune); i++ {
-		if stringRune[i] == '\n' {
+		if stringRune[i] < 32 {
 			return false
 		}
 	}
