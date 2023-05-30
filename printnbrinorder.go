4e5ff7f04f5d846ccab848cfb36a87487b2c530a
The-Estonian <Zaar2213@mail.com>
2023-05-30 16:24:58 +0300

    Quest5/printnbrinorder edge case fixed

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -7,17 +7,17 @@ import (
 func PrintNbrInOrder(n int) {
 	numberSort := []int{}
 	for i := 0; i <= n; i++ {
-		if n > 10 {
+		if n >= 10 {
 			numberSort = append(numberSort, n%10)
-			n /= 10
-		} else {
+			n = n / 10
+			i--
+		} else if n < 10 {
 			numberSort = append(numberSort, n)
 			break
 		}
 	}
 	SortIntegerTable(numberSort)
-
 	for i := 0; i < len(numberSort); i++ {
-		z01.PrintRune(rune(numberSort[i]))
+		z01.PrintRune(rune(numberSort[i] + '0'))
 	}
 }
