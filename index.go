165c3970a57c46639c7fe0037d75d146c17b5dd0
The-Estonian <Zaar2213@mail.com>
2023-05-30 13:14:50 +0300

    Quest5/index sixth edge case fixed

--- index.go
+++ index.go
@@ -1,26 +1,25 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if len(s) < len(toFind) || len(s) == 0 || len(toFind) == 0 {
-		return -1
-	}
-	for i := 0; i < len(s); i++ {
-		if string(s[i]) == toFind {
-			return i
-		}
-		if string(s[i]) == string(toFind[0]) {
-			iteratorCounter := i + len(toFind) - 1
-			if rune(s[i-1]) == 34 {
-				iteratorCounter = len(toFind) + i - 1
-			}
-			counter := 0
-			for x := i; x <= iteratorCounter; x++ {
-				if string(s[x]) == string(toFind[x-i]) {
-					counter++
-				}
-			}
-			if counter == len(toFind) {
+	if len(s) > 0 && len(toFind) > 0 && len(s) > len(toFind) {
+		for i := 0; i < len(s); i++ {
+			if string(s[i]) == toFind {
 				return i
+			}
+			if string(s[i]) == string(toFind[0]) {
+				iteratorCounter := i + len(toFind) - 1
+				if rune(s[i-1]) == 34 {
+					iteratorCounter = len(toFind) + i - 1
+				}
+				counter := 0
+				for x := i; x <= iteratorCounter; x++ {
+					if string(s[x]) == string(toFind[x-i]) {
+						counter++
+					}
+				}
+				if counter == len(toFind) {
+					return i
+				}
 			}
 		}
 	}
