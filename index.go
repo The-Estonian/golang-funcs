bf3e46daba65ccf9567374a9e5f13a277eb310dc
The-Estonian <Zaar2213@mail.com>
2023-05-30 13:30:55 +0300

    Quest5/index ninth edge case fixed

--- index.go
+++ index.go
@@ -1,22 +1,26 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if len(s) > 0 && len(toFind) > 0 && len(s) > len(toFind) {
-		for i := 0; i < len(s); i++ {
-			if string(s[i]) == toFind {
-				return i
+	if len(toFind) == 0 {
+		return 0
+	}
+	if len(toFind) > len(s) {
+		return -1
+	}
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) == toFind {
+			return i
+		}
+		if string(s[i]) == string(toFind[0]) {
+			iteratorCounter := i + len(toFind) - 1
+			counter := 0
+			for x := i; x <= iteratorCounter; x++ {
+				if string(s[x]) == string(toFind[x-i]) {
+					counter++
+				}
 			}
-			if string(s[i]) == string(toFind[0]) {
-				iteratorCounter := i + len(toFind) - 1
-				counter := 0
-				for x := i; x <= iteratorCounter; x++ {
-					if string(s[x]) == string(toFind[x-i]) {
-						counter++
-					}
-				}
-				if counter == len(toFind) {
-					return i
-				}
+			if counter == len(toFind) {
+				return i
 			}
 		}
 	}
