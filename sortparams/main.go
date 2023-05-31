670e42b236832b6ac54b9903f035796ff67762a3
The-Estonian <Zaar2213@mail.com>
2023-05-31 13:33:35 +0300

    Quest6/sortparams started

---
+++ sortparams/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args[1:]
+	for i := 0; i < len(arguments); i++ {
+		for j := 0; j < len(arguments); j++ {
+			if arguments[i] < arguments[j] {
+				arguments[i], arguments[j] = arguments[j], arguments[i]
+			}
+		}
+	}
+	for x := 0; x < len(arguments); x++ {
+		z01.PrintRune(rune(arguments[x][0]))
+		z01.PrintRune('\n')
+	}
+
+}
