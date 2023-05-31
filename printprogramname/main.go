efaadb56a21b76d9286ed0eb051f1cd58ddc9a49
The-Estonian <Zaar2213@mail.com>
2023-05-31 09:58:44 +0300

    Quest6/printprogramname started

---
+++ printprogramname/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arguments := os.Args[0][2:]
+	for i := 0; i < len(arguments); i++ {
+		z01.PrintRune(rune(arguments[i]))
+	}
+	z01.PrintRune(rune('\n'))
+}
