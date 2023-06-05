712f0aef204126b21cea5f655e70c1d82066feca
The-Estonian <Zaar2213@mail.com>
2023-06-05 16:17:42 +0300

    Quest8/cat edge case 3

--- cat/main.go
+++ cat/main.go
@@ -14,8 +14,8 @@ func printStr(s string) {
 }
 
 func main() {
-	args := os.Args
-	if len(args) > 1 {
+	args := os.Args[1:]
+	if len(args) > 0 {
 		for i := 0; i < len(args); i++ {
 			text, err := ioutil.ReadFile(args[i])
 			if err != nil {
@@ -24,14 +24,13 @@ func main() {
 			}
 			z01.PrintRune('\n')
 			printStr(string(text))
+			printStr("\n")
 		}
 	} else {
-	}
-	x := 0
-	for x < 1 {
-		buffer := make([]byte, 1024)
-		ui, _ := os.Stdin.Read(buffer)
-		printStr(string(buffer[:ui]))
-		x++
+		for {
+			buffer := make([]byte, 1024)
+			sentence, _ := os.Stdin.Read(buffer)
+			printStr(string(buffer[:sentence]))
+		}
 	}
 }
