ce73e382e82134c5890d03f6dc8b982745214cb6
The-Estonian <Zaar2213@mail.com>
2023-06-05 23:01:54 +0300

    Quest8/ztail edge case 50

--- ztail/main.go
+++ ztail/main.go
@@ -65,10 +65,9 @@ func main() {
 				fmt.Printf("==> %v <==\n", i)
 				if len(text) < BasicAtoi(number) {
 					if text[len(text)-1] == '\n' {
-						fmt.Printf("%s\n", text[:len(text)-1])
-					} else {
-						fmt.Printf("%s\n", text)
+						text = text[:len(text)-1]
 					}
+					fmt.Printf("%s\n", text)
 					os.Exit(1)
 				} else {
 					fmt.Printf("%s\n", text[len(text)-BasicAtoi(number):])
