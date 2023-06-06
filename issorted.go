53c169194e80b00bf155c5d36ea40d6c39df39a1
The-Estonian <Zaar2213@mail.com>
2023-06-06 11:44:35 +0300

    Quest9/issorted fixed edge case 3

--- issorted.go
+++ issorted.go
@@ -3,26 +3,28 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	ascending := true
 	answer := true
-	if a[0] > a[1] {
-		ascending = false
-	}
-	for x := 0; x < len(a); x++ {
-		for y := x + 1; y < len(a); y++ {
-			if ascending {
-				if f(a[x], a[y]) < 0 {
-					answer = true
-				} else if f(a[x], a[y]) == 0 {
-					continue
+	if len(a) > 1 {
+		if a[0] > a[1] {
+			ascending = false
+		}
+		for x := 0; x < len(a); x++ {
+			for y := x + 1; y < len(a); y++ {
+				if ascending {
+					if f(a[x], a[y]) < 0 {
+						answer = true
+					} else if f(a[x], a[y]) == 0 {
+						continue
+					} else {
+						return false
+					}
 				} else {
-					return false
-				}
-			} else {
-				if f(a[x], a[y]) > 0 {
-					answer = true
-				} else if f(a[x], a[y]) == 0 {
-					continue
-				} else {
-					return false
+					if f(a[x], a[y]) > 0 {
+						answer = true
+					} else if f(a[x], a[y]) == 0 {
+						continue
+					} else {
+						return false
+					}
 				}
 			}
 		}
