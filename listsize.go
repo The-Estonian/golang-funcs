adfc7ccf8537b9839cb74747607fe7f30da84b8f
The-Estonian <Zaar2213@mail.com>
2023-06-08 10:25:12 +0300

    Quest10/listsize edge case fix

--- listsize.go
+++ listsize.go
@@ -2,6 +2,9 @@ package piscine
 
 func ListSize(l *List) int {
 	counter := 1
+	if l.Head == nil {
+		return 0
+	}
 	iterator := l.Head
 	for ; iterator.Next != nil; iterator = iterator.Next {
 		counter++
