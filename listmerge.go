5b9c588a9e703cae68dc59716d512e93a8841c77
The-Estonian <Zaar2213@mail.com>
2023-06-08 22:16:48 +0300

    Quest10/listmerge edge case fixed

--- listmerge.go
+++ listmerge.go
@@ -1,13 +1,16 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	firstLooper := l1.Head
 	secondLooper := l2.Head
 	for ; secondLooper != nil; secondLooper = secondLooper.Next {
+		firstLooper := l1.Head
 		insertData := &NodeL{Data: secondLooper.Data}
-		for firstLooper.Next != nil {
-			firstLooper = firstLooper.Next
+		if l1.Head == nil {
+			l1.Head = insertData
+		} else {
+			for ; firstLooper.Next != nil; firstLooper = firstLooper.Next {
+			}
+			firstLooper.Next = insertData
 		}
-		firstLooper.Next = insertData
 	}
 }
