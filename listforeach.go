1dba41c9aae53f332276acaff5848deb0e4d983a
The-Estonian <Zaar2213@mail.com>
2023-06-08 16:00:33 +0300

    Quest10/listforeach started

---
+++ listforeach.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
+
+func ListForEach(l *List, f func(*NodeL)) {
+	looper := l.Head
+	for ; looper != nil; looper = looper.Next {
+		f(looper)
+	}
+}
