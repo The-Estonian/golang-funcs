3ab33104a9cdb06c4d70c69b2aa9b54e5c67a343
The-Estonian <Zaar2213@mail.com>
2023-06-09 08:25:20 +0300

    Quest10/listsort started

---
+++ listsort.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ListSort(l *NodeI) *NodeI {
+	for firstIndex := l; firstIndex != nil; firstIndex = firstIndex.Next {
+		for secondIndex := firstIndex.Next; secondIndex != nil; secondIndex = secondIndex.Next {
+			if firstIndex.Data > secondIndex.Data {
+				firstIndex.Data, secondIndex.Data = secondIndex.Data, firstIndex.Data
+			}
+		}
+	}
+	return l
+}
