f3a0f8875f67a5e38c602ea2c67d7753e89164ac
The-Estonian <Zaar2213@mail.com>
2023-06-10 08:59:11 +0300

    Quest10/sortlistinsert started

---
+++ sortlistinsert.go
@@ -0,0 +1,22 @@
+package piscine
+
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+	input := &NodeI{Data: data_ref}
+	nodeLooper := l
+	if l == nil {
+		l = input
+	} else {
+		if nodeLooper.Data > input.Data {
+			input.Next = nodeLooper
+			nodeLooper = input
+			return nodeLooper
+		}
+		for ; nodeLooper.Data < data_ref; nodeLooper = nodeLooper.Next {
+			if nodeLooper.Next.Data > data_ref {
+				input.Next = nodeLooper.Next
+				nodeLooper.Next = input
+			}
+		}
+	}
+	return l
+}
